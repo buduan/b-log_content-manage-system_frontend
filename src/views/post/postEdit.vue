ab415990ef7e4da1c3bce30d60acaeb1d5e9c287
不断同学 <buduan461@qq.com>
2025-01-04 21:56:58 +0800

    fix: postEdit.vue Initalize template

---
+++ src/views/post/postEdit.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div id="articleTitle" class="m-16">
+    <h1>文章标题</h1>
+  </div>
+
+  <div id="articleContent" class="p-16">
+    <p>文章内容</p>
+    <img class="h-24 w-auto" src="https://via.placeholder.com/1280x720" alt="article image" />
+  </div>
+</template>
